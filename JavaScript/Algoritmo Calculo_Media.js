69ce2fb2bd05983649c0c5b683801a23c1a57851
Francisco Mendes <fmmagalhaesfilho@aluno.uespi.br>
2022-02-21 00:28:27 -0300

    JavaScript Codes

---
+++ JavaScript/Algoritmo Calculo_Media.js
@@ -0,0 +1,14 @@
+Algoritmo Calculo_Media
+Var 
+  N1, N2 , N3, MEDIA : real
+Inínio
+  Leia N1, N2 , N3
+  MEDIA = (N1 + N2 + N3)/3
+  Se MEDIA >= 7 então
+    Escreva "Aprovado!"
+  E se MEDIA < 7 e MEDIA >= 4 então 
+    Escreva "Exame Final!"
+  Senão
+    Escreva "Reprovado!"
+  Fim_se
+Fim	 
---
+++ JavaScript/If_Else.js
@@ -0,0 +1,9 @@
+var x;
+x = prompt('Digite um número: ');
+x = Number(x);
+console.log();
+if (x%2==0){
+  console.log(x+' é um número par!!!');
+}else{
+  console.log(x+' é um número ímpar!!!');
+}
